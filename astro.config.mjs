356c1c3c15f469b4d8beae459e411b8d1b910d59
leonnolf <leon.nolf@student.howest.be>
2025-11-07 11:49:26 +0100

    blablabla

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 
 export default defineConfig({
-    site: 'https://github.com/leonnolf/cck-site-start.git',
+    site: 'https://cck.leonnolf.github.io',
     base: '/my-repo',
 });
