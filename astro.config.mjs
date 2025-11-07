c3e1c24e8b7ed202235216f2e5de751bf59a6802
leonnolf <leon.nolf@student.howest.be>
2025-11-07 12:02:29 +0100

    Add new section to README

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 
 export default defineConfig({
-    site: 'https://leonnolf.github.io/cck-site-start/',
+    site: 'https://cck.leonnolf.github.io',
     base: '/my-repo',
 });
